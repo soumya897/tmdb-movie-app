726ae90db5e79b27182bef1d8b632af38ae4d7be
Soumya Ghosh <soumyaghosh825@gmail.com>
2025-07-12 10:32:15 +0530

    Update

--- front-end/src/pages/home/home.jsx
+++ front-end/src/pages/home/home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     categories.forEach(async ({ path }) => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/${path}?api_key=6e5c5ee5feedc953d504088b213370e5&language=en-US`
+          `https://api.themoviedb.org/3/movie/${path}?api_key=API_KEY&language=en-US`
         )
         const data = await res.json()
         setMovies((prev) => ({ ...prev, [path]: data.results }))
@@ -33,7 +33,7 @@ const Home = () => {
     const fetchCarousel = async () => {
       try {
         const res = await fetch(
-          "https://api.themoviedb.org/3/movie/popular?api_key=6e5c5ee5feedc953d504088b213370e5&language=en-US"
+          "https://api.themoviedb.org/3/movie/popular?api_key=API_KEY&language=en-US"
         )
         const data = await res.json()
         setCarouselMovies(data.results)
